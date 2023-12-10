859d3a2b44ad641f38bd0d55e3bc11f616d6beed
Fatih <yildizafatih@gmail.com>
2023-12-10 14:19:22 +0100

    refactored code to use classes

--- script.js
+++ script.js
@@ -1,21 +1,80 @@
-/* Book object constructor */
-function Book(author, title, numOfPages, isRead, bookContainer) {
-    this.author = author;
-    this.title = title;
-    this.numOfPages = numOfPages;
-    this.isRead = isRead;
-    this.bookContainer = bookContainer;
+// Book class  
+class Book {
+    #bookContainer;
+
+    constructor(author, title, numOfPages, isRead) {
+        this.author = author;
+        this.title = title;
+        this.numOfPages = numOfPages;
+        this.isRead = isRead;
+        this.#bookContainer = document.createElement("div");
+    }
+    // returns a new book container with the given input  
+    createContainer() {
+        let authorElement = document.createElement("p");
+        authorElement.textContent = "author: " + this.author;
+
+        let titleElement = document.createElement("p");
+        titleElement.textContent = "title: " + this.title;
+
+        let numOfPagesElement = document.createElement("p");
+        numOfPagesElement.textContent = this.numOfPages + " pages";
+
+        let isReadButton = document.createElement("button");
+        isReadButton.className = "read-button";
+        isReadButton.textContent = this.isRead ? "read" : "not read";
+        isReadButton.style.backgroundColor = this.isRead ? "rgb(99, 218, 99)" : "rgb(224, 79, 99)";
+
+        let removeButton = document.createElement("button");
+        removeButton.className = "remove-button";
+        removeButton.textContent = "remove book";
+
+        this.#bookContainer.append(authorElement, titleElement, numOfPagesElement, isReadButton, removeButton);
+        return this.#bookContainer;
+    }
+    // returns the book container
+    getBookContainer() {
+        return this.#bookContainer;
+    }
+}
+// Library class
+class Library {
+    #myLibrary;
+
+    constructor() {
+        // array of Book objects
+        this.#myLibrary = [];
+    }
+
+    getLibrarySize() {
+        return this.#myLibrary.length;
+    }
+
+    addToLibrary(book) {
+        this.#myLibrary.push(book);
+    }
+
+    getBook(index) {
+        return this.#myLibrary[index];
+    }
+
+    removeBook(index) {
+        // removes from the DOM
+        this.getBook(index).getBookContainer().remove();
+        // removes from the library array
+        this.#myLibrary.splice(index, 1);
+    }
 }
 
-/* array of Book objects */
-let myLibrary = [];
+// creates a library
+let library = new Library();
 
-/* elements already in the DOM */
+// elements already in the DOM  
 let booksContainer = document.querySelector("#books-container");
 let addBookButton = document.querySelector("#add-book");
 let dialog = document.querySelector("dialog");
 
-/* event listeners for the elements that do not exist yet */
+// event listeners for elements that gets created later  
 document.addEventListener("click", function (event) {
     if (event.target.id === "submit-button") {
         submitHandler();
@@ -26,89 +85,62 @@ document.addEventListener("click", function (event) {
     }
 })
 
-/* event listener for the Add Book button */
+// event listener for the Add Book button  
 addBookButton.addEventListener("click", addBookHandler);
 
-/* seperate function to handle the Add Book button */
+// seperate function to handle the Add Book button  
 function addBookHandler() {
-    /* shows the dialog */
+    // shows the dialog  
     dialog.showModal();
-    /* prevents page refresh */
+    // prevents page refresh  
     document.querySelector("#add-book-form").addEventListener("submit", function (event) {
         event.preventDefault();
     })
 }
 
-/* seperate function to handle the Submit button */
+// seperate function to handle the Submit button  
 function submitHandler() {
-    /* gets the input values and creates a container */
+    // gets the input values  
     let author = document.querySelector("#author-input").value;
     let title = document.querySelector("#title-input").value;
     let numOfPages = document.querySelector("#pages-input").value;
     let isRead = document.querySelector("#read-input").checked;
-    let bookContainer = createBookContainer(author, title, numOfPages, isRead);
-    /* creates a new Book object and adds it to the library */
-    let newBook = new Book(author, title, numOfPages, isRead, bookContainer);
-    myLibrary.push(newBook);
-    /* resets the form */
+    // creates a new Book object and a container  
+    let newBook = new Book(author, title, numOfPages, isRead);
+    library.addToLibrary(newBook);
+    let bookContainer = newBook.createContainer();
+    // resets the form  
     document.querySelector("#add-book-form").reset();
-    /* closes the dialog and adds the book to the DOM */
+    // closes the dialog and adds the book to the DOM  
     dialog.close();
     booksContainer.appendChild(bookContainer);
 }
-/* seperate function to handle the Remove button */
+// seperate function to handle the Remove button  
 function removeHandler(event) {
-    /* removes the book from the library */
+    // removes the book from the library  
     let bookContainerToRemove = event.target.parentElement;
     let indexToRemove;
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].bookContainer === bookContainerToRemove) {
+    for (let i = 0; i < library.getLibrarySize(); i++) {
+        if (library.getBook(i).getBookContainer() === bookContainerToRemove) {
             indexToRemove = i;
         }
     }
-    myLibrary.splice(indexToRemove, 1);
-    /* removes the book from the DOM */
-    bookContainerToRemove.remove();
+    library.removeBook(indexToRemove);
 }
-/* seperate function to handle the Read/Not Read button */
+// seperate function to handle the Read/Not Read button  
 function readButtonHandler(event) {
     let bookContainer = event.target.parentElement;
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].bookContainer === bookContainer) {
+    for (let i = 0; i < library.getLibrarySize(); i++) {
+        if (library.getBook(i).getBookContainer() === bookContainer) {
             if (event.target.textContent === "read") {
-                myLibrary[i].isRead = false;
+                library.getBook(i).isRead = false;
                 event.target.textContent = "not read";
                 event.target.style.backgroundColor = "rgb(224, 79, 99)";
             } else if (event.target.textContent === "not read") {
-                myLibrary[i].isRead = true;
+                library.getBook(i).isRead = true;
                 event.target.textContent = "read";
                 event.target.style.backgroundColor = "rgb(99, 218, 99)";
             }
         }
     }
-}
-/* returns a new book container with the given input */
-function createBookContainer(author, title, numOfPages, isRead) {
-    let bookContainer = document.createElement("div");
-
-    let authorElement = document.createElement("p");
-    authorElement.textContent = "author: " + author;
-
-    let titleElement = document.createElement("p");
-    titleElement.textContent = "title: " + title;
-
-    let numOfPagesElement = document.createElement("p");
-    numOfPagesElement.textContent = numOfPages + " pages";
-
-    let isReadButton = document.createElement("button");
-    isReadButton.className = "read-button";
-    isReadButton.textContent = isRead ? "read" : "not read";
-    isReadButton.style.backgroundColor = isRead ? "rgb(99, 218, 99)" : "rgb(224, 79, 99)";
-
-    let removeButton = document.createElement("button");
-    removeButton.className = "remove-button";
-    removeButton.textContent = "remove book";
-
-    bookContainer.append(authorElement, titleElement, numOfPagesElement, isReadButton, removeButton);
-    return bookContainer;
 }
